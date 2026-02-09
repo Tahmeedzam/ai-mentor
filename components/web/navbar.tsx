7f62d82bbe78a5f32a1973909dd20b6d6e85c6f6
Chinmay-swnt <chinmayswnt.01@gmail.com>
2026-02-09 18:08:09 +0530

    merge

--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -2,16 +2,19 @@ import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 
 export default function NavigationBar() {
-  
-
   return (
-    
-        <div className='w-full  h-1/12 flex justify-end bg-gray-950'>
-            <div className="flex gap-5 items-center pr-6">
-            <Link href='/' className={buttonVariants({variant:'default'})}>Login</Link>
-            <Link href='/' className={buttonVariants({variant:"secondary"})}>Signup</Link>
-            </div>
-        </div>
-    
+    <div className="w-full  h-1/12 flex justify-end bg-gray-950">
+      <div className="flex gap-5 items-center pr-6">
+        <Link href="/" className={buttonVariants({ variant: "default" })}>
+          Login
+        </Link>
+        <Link
+          href="/sign-up"
+          className={buttonVariants({ variant: "secondary" })}
+        >
+          Signup
+        </Link>
+      </div>
+    </div>
   );
 }
